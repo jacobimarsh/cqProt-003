ad2b57f2932161aad4b1a38ef421ce85067a4d40
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-09-29 13:38:15 +0800

    Update 03-LDBlockShow.sh

--- 03-LDBlockShow.sh
+++ 03-LDBlockShow.sh
@@ -4,4 +4,5 @@ awk -F "," '31500000<$3 && $3<31950000 {print $2,$1,$8}' protein.FarmCPU.csv | s
 
 ##RUN
 LDBlockShow/bin/LDBlockShow -InVCF fin_b51_protein_Gm20.SNPs.id.biallic_maf_0.01_geno_0.1.vcf.gz -OutPut GWAS_imp_R2 -OutPdf -Region 20:31500000:31950000 -SeleVar 2 -InGWAS ingwas.txt -InGFF in.gff3 -TopSite
+LDBlockShow/bin/LDBlockShow -InVCF fin_b51_protein_Gm20.SNPs.id.biallic_maf_0.01_geno_0.1.vcf.gz -OutPut GWAS_imp_DP_CI -OutPdf -Region 20:31550000:31850000 -SeleVar 1 -BlockType 1 -InGWAS ingwas.txt -InGFF in.gff3 -TopSite
 ##clean up in illustrator
