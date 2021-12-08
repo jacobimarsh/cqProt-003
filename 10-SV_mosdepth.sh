a8f3a7d8ad40d74f8cd4e7b65775953d18333e28
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-12-08 12:39:22 +0800

    Update 10-SV_mosdepth.sh

--- 10-SV_mosdepth.sh
+++ 10-SV_mosdepth.sh
@@ -3,7 +3,15 @@ do mosdepth -b 1 -Q 10 ${line} ${line}.chr20.bam
 zcat ${line}.regions.bed.gz | grep glyma.Wm82.gnm4.Gm20 > ${line}.chr20_regions.txt 
 done
 #probably better to do below by extracting a range with awk rather than this manual approach
-cat pop.txt | while read line; do grep -A 5034 31724592 ${line}.chr20_regions.txt > ${line}.reg.tt; done
+cat pop.txt | while read line; 
+do grep -A 5034 31724592 ${line}.chr20_regions.txt > ${line}.reg.tt; 
+done
+#
+cat pop.txt | while read line; 
+do grep -A 302 31728620 ${line}.reg.tt | awk '{sum+=$NF; n++} END { if (n>0) print sum / n; }' | xargs -I [] echo -e "${line}\t[]"; 
+done > 304bp_avs.txt
+sed -i 's/-/./g' 304bp_avs.txt
+sed -i 's/_/./g' 304bp_avs.txt
 # Below gives a quick summary of individuals with '0' runs of coverage, 
 # However some individuals have ~0-1 coverage in small sections of the region likely due to misassembly which significantly truncates the '0' counts
 # For this reason we manually inspected individuals who had significant 0counts that were lower than 310bp
