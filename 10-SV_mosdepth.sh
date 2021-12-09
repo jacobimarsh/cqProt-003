9c31bc65c9e58018401b195dd0446909a4431dc5
Jacob Marsh <51686617+JacobIMarsh@users.noreply.github.com>
2021-12-09 13:50:05 +0800

    Update 10-SV_mosdepth.sh

--- 10-SV_mosdepth.sh
+++ 10-SV_mosdepth.sh
@@ -20,3 +20,46 @@ awk '{print $4}' ${line}.reg.tt | uniq -c | sort -k1 | tail -n 1 >> 0sum.txt
 done
 grep -B 1 ' 0' 0sum.txt | grep -v ' 0' | grep -v -- -- > just0s.txt
 grep ' 0' 0sum.txt | awk '{print $1}' | paste just0s.txt - > 0counts.txt
+
+R
+library(tidyverse)
+library(pals)
+library(ggsci)
+
+avcov <- read.csv("304bp_avs.txt", sep = "\t", header = F) %>% as.tibble()
+haps <- read.csv("haps.txt", sep = "\t", header = F) %>% as.tibble()
+hapavcov <- merge(x = avcov, 
+               y = haps, 
+               by.x = "V1", 
+               by.y = "V2") %>% rename("ID" = "V1", "bpcov" = "V2", "hap" = "V1.y", "grp" = "V3")
+
+npg_col = pal_npg("nrc")(9)
+col_list2 <- c('wt'=npg_col[8],
+              'lr' = npg_col[3],
+              'ocult' =npg_col[2],
+              'mcult' =npg_col[4])
+
+bpcovplot <- ggplot(data = hapavcov %>% 
+  mutate(grp=factor(grp,
+                           levels = c('wt','lr','ocult','mcult')))
+               ) +
+  geom_jitter(aes(x = hap,
+                  y = bpcov,
+                  fill = grp),
+                  alpha=0.4, 
+              pch=21, 
+              width=0.2) +
+  scale_fill_manual("Grp", values = col_list2, labels = rev(c("Modern Cultivar", "Old Cultivar","Landrace" , "Wild-Type"))) +
+  xlab("Haplotype Group") +
+  ylab("Mean Depth Across SV") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust= 0.5),
+        legend.title = element_blank())
+
+ggsave('bpcovplot.pdf',
+       bpcovplot, 
+       device = 'pdf', 
+       dpi = 1800,
+       height = 90,
+       width = 160,
+       units = "mm")
